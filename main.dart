68ae0ad7689752541514bc4b64b6711690365156
drxshtx <117944730+drxshtx@users.noreply.github.com>
2023-07-20 23:54:43 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,100 +1,160 @@
+import 'dart:html';
+
+import 'object.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CounterApp());
+  runApp(MaterialApp(
+    home: budget(),
+  ));
+}
+class budget extends StatefulWidget {
+  const budget({super.key});
+
+  @override
+  State<budget> createState() => _budgetState();
 }
 
-class CounterApp extends StatelessWidget {
+class _budgetState extends State<budget> {
+  int expense=26700;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Counter App',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: CounterPage(),
-    );
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   double width = MediaQuery.of(context).size.width;
-  //   double height= MediaQuery.of(context).size.height;
-  // }
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height= MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
-      ),
-      body:SizedBox(
-        width: width,
-        height: height,
-        child: Stack(
-          children: [
-            Align(
-                alignment: Alignment.center,
-                child:Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  Text(
-                   'Counter Value:',
-                    style: TextStyle(fontSize: 20),
-                   ),
-                  Text(
-                   '$_counter',
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-              ], ),
-            ),
-            Align(
-                alignment: Alignment.bottomRight,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FloatingActionButton(
-                        onPressed: _decrementCounter,
-                        tooltip: 'Decrement',
-                        child: Icon(Icons.remove),
-                        // backgroundColor: Colors.teal[200],
-
-                      ),
-                      SizedBox(width: 10),
-                      FloatingActionButton(
-                        onPressed: _incrementCounter,
-                        tooltip: 'Increment',
-                        child: Icon(Icons.add),
-                      )
-                    ],
-                  )
-                ),
-          ],
+        title: Text(
+          'Budget Tracker',
         ),
+        centerTitle: true,
+        backgroundColor: Colors.purple[400],
+      )
+          ,body:Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Icon(Icons.person_3_rounded, size:50),
+                ),
+                SizedBox(height:30),
+                Text(
+                  'Welcome Back !!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(height:20),
+                GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => page2()));
+                  },
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Total Expense  :',
+                              style: TextStyle(
+                                fontSize:20,
+                              ),
+                        ),
+                        SizedBox(width:10),
+                        Text(
+                          '$expense',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )
+                        ),
+                        Icon(Icons.arrow_drop_down,size:30),
+
+                      ],
+                    )
+                  ),
+                ),
+              ],
+            ),
+    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.purpleAccent,
+        child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+class page2 extends StatefulWidget {
+  const page2({super.key});
+
+  @override
+  State<page2> createState() => _page2State();
+}
+
+class _page2State extends State<page2> {
+  String category='';
+  late TextEditingController controller1;
+  @override
+  void initState() {
+    super.initState();
+    controller1 = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller1.dispose();
+    super.dispose();
+  }
+
+    @override
+  Future openDialog() => showDialog(
+     context: context,
+      builder: (context)=>AlertDialog(
+        title: Text( ' New Entry'),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: 'Category'),
+              controller: controller1,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Amount'),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+              onPressed: (){
+                Navigator.of(context).pop(controller1);
+                controller1.clear();
+              },
+              icon: Icon(Icons.add))
+        ],
+      ));
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Budget Tracker',
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.purple[400],
+        ),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: items.map((item)=>Text('${item.category} :  ${item.amount}')).toList(),
+      ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purpleAccent,
+        child: Icon(Icons.add),
+        onPressed: ()async{
+          final category = await openDialog();
+          setState(() {
+            this.category = category as String;
+          }
+          );
+        },
+      ),   );
   }
 }
